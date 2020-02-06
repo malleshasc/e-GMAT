64d66057c29ee3e9e90339b1f7c64ac6746ccb10
malleshasc <scmallesha@gmail.com>
2020-02-06 23:55:45 +0530

    some changes

--- quiz_generator_old.py
+++ quiz_generator_old.py
@@ -1,35 +1,84 @@
 """
 Quiz generator.
-name: Quiz_generator.py
+name: Quiz_generator_old.py
 date: 6th Feb 2020
 author: Mallesha SC
 """
-import random 
-
+#Quiz_generator Function generate the quizzes as per given conditions
 def Quiz_generator(input_file):
     #read the file contetnts to list using readlines method
     with open(input_file, 'r')as f:
         data = f.readlines()
-    data = list(set(data))   
+    #segregate the data as per difficulty level
+    easy_q = []
+    med_q = []
+    hard_q = []
+    for question in data:
+        if 'EASY' in question:
+            easy_q.append(question)
+        elif 'MEDIUM' in question:
+            med_q.append(question)
+        elif 'HARD' in question:
+            hard_q.append(question)
+    #quiz_list will contain all possible quizzes
     quiz_list = []
-    data_len = len(data)//10
+    tags = ['Tag1','Tag2','Tag3','Tag4','Tag5','Tag6']
     #while loop executes till it generates maximum possible quizzes with 6 question per quiz
-    while len(quiz_list) < data_len:
-        quiz = []
-        tags = []
-        diff_dict = {'EASY':0,'MEDIUM':0,'HARD':0}
-        while len(quiz)<6:  
-            question = random.choice(data)
-            tag_id,diff_id = question.split('|')[2],question.split('|')[1]
-            if tag_id not in tags and diff_dict[diff_id]!=2:
+    while len(quiz_list) < len(data)//10:  
+        t1,t2,t3,t4,t5,t6 = False,False,False,False,False,False       
+        quiz  = []  
+        x = 0
+        for question in easy_q:
+            if tags[0] in question and not t1:
                 quiz.append(question)
-                data.remove(question)
-                tags.append(tag_id)
-                diff_dict[diff_id] += 1
+                easy_q.remove(question)
+                t1 = True
+                x+=1
+            elif tags[1] in question and not t2:
+                quiz.append(question)
+                easy_q.remove(question)
+                t2 = True
+                x+=1
+            if x ==2:
+                break
+        for question in med_q:
+            if tags[2] in question and not t3:
+                quiz.append(question)
+                med_q.remove(question)
+                t3 = True
+                x+=1
+            elif tags[3] in question and not t4:
+                quiz.append(question)
+                med_q.remove(question)
+                t4 = True
+                x+=1
+            if x ==4 :
+                break
+        for question in hard_q:
+            if tags[4] in question and not t5:
+                quiz.append(question)
+                hard_q.remove(question)
+                t5 = True
+                x+=1
+            elif tags[5] in question and not t6:
+                quiz.append(question)
+                hard_q.remove(question)
+                t6 = True
+                x+=1
+            if x ==6 :
+                break
         quiz_list.append(quiz)
-    data = list(set(data))   
-    for quiz in quiz_list:
-        quiz.extend(data[:4])
+        #Shuffle the tags to pick questions uniformly from all difficulty levels
+        tags = tags[2:] + tags[:2]
+    '''                     
+    At this point each quiz in quiz_list has 6 questions.
+    Randomly append the remaining questions to each quiz (4 questions each)
+    so the total number of questions will become 10 per quiz
+    and return quiz_list, number of quizzes generated
+    '''
+    data = list(set(easy_q+med_q+hard_q))
+    for question in quiz_list:
+        question.extend(data[:4])
         data = data[4:]
     return quiz_list,len(quiz_list)
 
@@ -41,7 +90,6 @@ def main():
     quizzes,no_of_quiz = Quiz_generator(file_name)
     print('----------------------------------------')
     print("Valid quizzes generated:",no_of_quiz)     
-    print('----------------------------------------')             
-
+    print('----------------------------------------') 
 if __name__ == '__main__':
     main()
